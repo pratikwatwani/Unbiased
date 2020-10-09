4ff9e2ed78c1804c0d6b43d98554878710435e42
Pratik Watwani <pratikpwatwani@gmail.com>
2020-10-09 17:36:19 -0400

    added frontend piece

---
+++ src/frontend/ebwe.py
@@ -0,0 +1,64 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import base64
+
+
+external_stylesheets = ['main.css', 'layout.css']
+logo_location = 'assets/logo.png'
+encoded_image = base64.b64encode(open(logo_location, 'rb').read()).decode('ascii')
+slider_marks_color = 'black'
+app = dash.Dash('EBWE',external_stylesheets=external_stylesheets)
+
+options = {}
+    #Get countries from the data
+    # Actor2Geo_FullName == 1
+    # Actor2Geo_CountryCode
+    # This will get countries, filter out unique
+
+
+app.layout = html.Div([
+        html.Img(src='data:image/png;base64,{}'.format(encoded_image), className = 'logo'),
+        html.H1("UNBIASED", className = 'title'),
+        html.H3(["Spatio-Temporal Event Based", html.Br(), "Wikipedia Activity"], className = 'tagline'),
+        html.H3("What's poppin? Don't mind me just watchin! 👀", className='subtitle'),
+        html.Div(
+            html.P(['Instructions:',html.Br(),'First select a country of your choice to filter the relevant articles',html.Br(), 'Then proceed to selecting a time period to show edit activity on that page.']), className='instructionpanel'
+        ), 
+        html.Div(
+            children = [dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        searchable=True, 
+        className='country-dropdown',
+        placeholder="Select a country",
+        ),    
+    ]),
+    html.Br(),
+    dcc.Slider(
+    min=30,
+    max=1825,
+    step=None,
+    marks={
+        30: {'label' :'30 Days', 'style': {'color': '#f50', 'fontFamily':'sans-serif', 'fontWeight':300}},
+        60: {'label' :'', 'style': {'color': slider_marks_color, 'fontFamily':'sans-serif', 'fontWeight':300}},
+        90: {'label' :'', 'style': {'color': slider_marks_color, 'fontFamily':'sans-serif', 'fontWeight':300}},
+        180: {'label' :'180 Days', 'style': {'color': slider_marks_color, 'fontFamily':'sans-serif', 'fontWeight':300}},
+        365: {'label' :'1 Years', 'style': {'color': slider_marks_color, 'fontFamily':'sans-serif', 'fontWeight':300}},
+        730: {'label' :'2 Years', 'style': {'color': slider_marks_color, 'fontFamily':'sans-serif', 'fontWeight':300}},
+        1095: {'label' :'3 Years', 'style': {'color': slider_marks_color, 'fontFamily':'sans-serif', 'fontWeight':300}},
+        1460: {'label' :'4 Years', 'style': {'color': slider_marks_color, 'fontFamily':'sans-serif', 'fontWeight':300}},
+        1825: {'label': '5 Years', 'style': {'color': '#f50', 'fontFamily':'sans-serif'}}
+    },
+    className='dateSlider',
+    tooltip={'placement': 'top', 'always_visible': False}
+)     
+])
+    
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+    marker_style = dict(color='#444', fontFamily='sans-serif', fontWeight=300)
